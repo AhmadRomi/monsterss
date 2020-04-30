abc17657d77cf05373f665290df5cdf18a1ff4f8
hamada romi <ahmed.romi2020@gmail.com>
2020-05-01 02:45:01 +0300

    Updates

---
+++ precache-manifest.00310aa4f30c52279175e5043f84c14f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845c3fbd1087e3713ebbbaa7b111b862",
+    "url": "/monsterss/index.html"
+  },
+  {
+    "revision": "46c2462dce3c950e3781",
+    "url": "/monsterss/static/css/main.59c05fbb.chunk.css"
+  },
+  {
+    "revision": "aac8c94dee885d426094",
+    "url": "/monsterss/static/js/2.504ce3f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterss/static/js/2.504ce3f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46c2462dce3c950e3781",
+    "url": "/monsterss/static/js/main.dfcb6c12.chunk.js"
+  },
+  {
+    "revision": "223527726876786aa748",
+    "url": "/monsterss/static/js/runtime-main.6484f204.js"
+  }
+]);
